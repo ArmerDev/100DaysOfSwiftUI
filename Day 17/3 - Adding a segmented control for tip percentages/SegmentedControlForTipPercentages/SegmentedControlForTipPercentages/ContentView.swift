1cfad687fb47f59604315d752653df3d202dedb2
ArmerDev <Armer.Developer@gmail.com>
2023-04-30 11:36:27 +0100

    Added Day 17 - Percentage Segmented Control

---
+++ Day 17/3 - Adding a segmented control for tip percentages/SegmentedControlForTipPercentages/SegmentedControlForTipPercentages/ContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  SegmentedControlForTipPercentages
+//
+//  Created by James Armer on 30/04/2023.
+//
+
+import SwiftUI
+
+/*
+ 
+ Below we will add a segmented control to select what percentage we would like to tip
+ 
+ */
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+/*
+ 
+ While we understand what the percentages mean on the segmented control, a user might not understand what it is for.
+ 
+ Let's add a header to the Section using header.
+ 
+ The below code in ContentView2 uses multiple trailing closures to specify both the section body (the first closure) and the second header (the second closure).
+ 
+ */
+
+struct ContentView2: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDisplayName("ContentView")
+        ContentView2()
+            .previewDisplayName("ContentView 2")
+    }
+}
---
+++ Day 17/3 - Adding a segmented control for tip percentages/SegmentedControlForTipPercentages/SegmentedControlForTipPercentages/SegmentedControlForTipPercentagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControlForTipPercentagesApp.swift
+//  SegmentedControlForTipPercentages
+//
+//  Created by James Armer on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControlForTipPercentagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
