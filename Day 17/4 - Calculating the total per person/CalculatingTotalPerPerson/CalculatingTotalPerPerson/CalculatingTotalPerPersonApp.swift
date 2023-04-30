3ac803441e13a79000627e6438289fc14670c07c
ArmerDev <Armer.Developer@gmail.com>
2023-04-30 12:10:59 +0100

    Added Day 17 - Calculating Total Per Person

---
+++ Day 17/4 - Calculating the total per person/CalculatingTotalPerPerson/CalculatingTotalPerPerson/CalculatingTotalPerPersonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatingTotalPerPersonApp.swift
+//  CalculatingTotalPerPerson
+//
+//  Created by James Armer on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatingTotalPerPersonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day 17/4 - Calculating the total per person/CalculatingTotalPerPerson/CalculatingTotalPerPerson/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  CalculatingTotalPerPerson
+//
+//  Created by James Armer on 30/04/2023.
+//
+
+import SwiftUI
+
+/*
+ 
+ To calculate the total per person, we will create a computed property call totalPerPerson, and pass that into the final Text view.
+ 
+ You should find that because all the values that make up our total are marked with @State, changing any of them will cause the total to be recalculated automatically.
+ 
+ */
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
