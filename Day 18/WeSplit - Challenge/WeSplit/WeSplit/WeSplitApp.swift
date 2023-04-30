e8f782867906d0db77f2ba943383402c5fcb1c0b
ArmerDev <Armer.Developer@gmail.com>
2023-04-30 16:36:52 +0100

    Added Day 18

---
+++ Day 18/WeSplit - Challenge/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by James Armer on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
