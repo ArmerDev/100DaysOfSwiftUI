f0ed1efdfd04d8eb427cfbcd97c81e60ddc02bdc
ArmerDev <Armer.Developer@gmail.com>
2023-05-02 21:56:15 +0100

    Added Day 21 - Stacking up buttons

---
+++ Day 21/1 - Stacking up buttons/StackingUpButtons/StackingUpButtons/StackingUpButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackingUpButtonsApp.swift
+//  StackingUpButtons
+//
+//  Created by James Armer on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct StackingUpButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
