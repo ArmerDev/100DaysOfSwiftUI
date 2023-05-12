e79bd6f0dd637dee2ac5e52a47ea372dea8c4428
ArmerDev <Armer.Developer@gmail.com>
2023-05-12 08:31:54 +0100

    Added Day 31

---
+++ Day 31/Word Scramble - Challenge/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by James Armer on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
