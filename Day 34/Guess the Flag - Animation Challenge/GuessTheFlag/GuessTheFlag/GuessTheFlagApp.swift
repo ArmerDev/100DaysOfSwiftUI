91efda9f875b01cf95b3859efd80e867bcb7f2c0
ArmerDev <Armer.Developer@gmail.com>
2023-05-14 08:31:45 +0100

    Added Day 34

---
+++ Day 34/Guess the Flag - Animation Challenge/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by James Armer on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
