720492a7c30c270a6464472ac95dbfea6749a01e
ArmerDev <Armer.Developer@gmail.com>
2023-05-17 21:28:59 +0100

    More work on Day 35 Challenge

--- Day 35/EdutainmentMultiplicationApp/EdutainmentMultiplicationApp/ContentView.swift
+++ Day 35/EdutainmentMultiplicationApp/EdutainmentMultiplicationApp/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var numberOfQuestions = 10
+    @State private var currentQuestionNumber = 0
+    @State private var playersAnswer = ""
     @State private var selectedTimesTable = 2
-    @State private var someArray: [qAndA] = []
+    @State private var someArray: [qAndA]? = []
     @State private var showingSettings = true
+    @State private var score = 0
+    @State private var showEndGame = false
     
     
     var body: some View {
@@ -22,38 +26,25 @@ struct ContentView: View {
                 .ignoresSafeArea()
             
             if showingSettings {
-                VStack {
-                    Text("Settings")
-                        .font(.largeTitle.bold())
-                    
-                    Stepper("\(selectedTimesTable) Times Table", value: $selectedTimesTable, in: 2...12)
-                    
-                    Stepper("\(numberOfQuestions) Questions", value: $numberOfQuestions, in: 5...15, step: 5)
-                    
-                    Button("Play") {
-                        createQuestions()
-                        showingSettings.toggle()
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                .padding()
-                .background(.ultraThinMaterial)
-                .cornerRadius(15)
-                .padding()
+                gameSetup
             } else {
-                Text("Insert Questions Here...")
-                    .foregroundColor(.white)
+                gameView
             }
         }
-        
-    }
+        .alert("End of Game", isPresented: $showEndGame, actions: {
+              Button("Play Again") { resetGame() }
+            }, message: {
+              Text("Your score was \(score)")
+            })
+        }
+    
     
     func createQuestions() {
         
         // TODO: Need to make it so that questions are not repeated!
         
         var questionArray = [qAndA]()
-    
+        
         for _ in 0...numberOfQuestions {
             let randomInt = Int.random(in: 1...20)
             questionArray.append(qAndA(question: "\(selectedTimesTable) X \(randomInt)", answer: "\(randomInt * selectedTimesTable)"))
@@ -67,6 +58,80 @@ struct qAndA: Identifiable {
     let id = UUID()
     let question: String
     let answer: String
+}
+
+extension ContentView {
+    var gameSetup: some View {
+        VStack {
+            Text("Settings")
+                .font(.largeTitle.bold())
+            
+            Stepper("\(selectedTimesTable) Times Table", value: $selectedTimesTable, in: 2...12)
+            
+            Stepper("\(numberOfQuestions) Questions", value: $numberOfQuestions, in: 5...15, step: 5)
+            
+            Button("Play") {
+                createQuestions()
+                showingSettings.toggle()
+                currentQuestionNumber += 1
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(15)
+        .padding()
+    }
+    
+    var gameView: some View {
+        VStack {
+            Text("Score: \(score)")
+                .font(.largeTitle)
+            
+            HStack {
+                Text("\(someArray![currentQuestionNumber].question) = ")
+                    .font(.title)
+                
+                TextField("", text: $playersAnswer)
+                    .foregroundColor(.black)
+                    .bold()
+                    .textFieldStyle(.roundedBorder)
+                    .frame(width: 75)
+            }
+            
+            Button("Next") {
+                if checkAnswerCorrect(playersAnswer: playersAnswer,
+                                      actualAnswer: someArray![currentQuestionNumber].answer) {
+                    score += 1
+                    nextQuestion()
+                } else {
+                    playersAnswer = ""
+                }
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .foregroundColor(.white)
+    }
+    
+    func checkAnswerCorrect(playersAnswer: String, actualAnswer: String) -> Bool {
+        playersAnswer == actualAnswer
+    }
+    
+    func nextQuestion() {
+        if currentQuestionNumber < numberOfQuestions {
+            currentQuestionNumber += 1
+            print(currentQuestionNumber)
+        } else {
+            showEndGame.toggle()
+        }
+    }
+    
+    func resetGame() {
+        showingSettings = true
+        playersAnswer = ""
+        currentQuestionNumber = 0
+        score = 0
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
