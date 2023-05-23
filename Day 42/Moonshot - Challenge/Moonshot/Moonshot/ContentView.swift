43c6c7614ef395ac82d51b4662f8974bfc3af19f
ArmerDev <Armer.Developer@gmail.com>
2023-05-23 21:42:49 +0100

    Added Day 42

---
+++ Day 42/Moonshot - Challenge/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by James Armer on 23/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    @State private var showAsGridView = true
+    
+    var body: some View {
+        NavigationView {
+            Group {
+                if showAsGridView {
+                    GridView(missions: missions, astronauts: astronauts)
+                } else {
+                    ListView(missions: missions, astronauts: astronauts)
+                }
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+            .toolbar {
+                Toggle(isOn: $showAsGridView) {
+                    Text("Grid View")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 42/Moonshot - Challenge/Moonshot/Moonshot/CrewRole.swift
@@ -0,0 +1,36 @@
+//
+//  CrewRole.swift
+//  Moonshot
+//
+//  Created by James Armer on 23/05/2023.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+    
+    var longFormattedLaunchDate: String {
+        launchDate?.formatted(date: .long, time: .omitted) ?? "N/A"
+    }
+}
