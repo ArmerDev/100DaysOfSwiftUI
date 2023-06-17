3e0567d872d6c4608e4cfb45e03e841fa9dd6cad
ArmerDev <Armer.Developer@gmail.com>
2023-06-17 23:52:33 +0100

    Preparing for Day 63

---
+++ Day 63/1 - Integrating Core Image with SwiftUI/IntegratingCoreImageWithSwiftUI/IntegratingCoreImageWithSwiftUI/IntegratingCoreImageWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegratingCoreImageWithSwiftUIApp.swift
+//  IntegratingCoreImageWithSwiftUI
+//
+//  Created by James Armer on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct IntegratingCoreImageWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
