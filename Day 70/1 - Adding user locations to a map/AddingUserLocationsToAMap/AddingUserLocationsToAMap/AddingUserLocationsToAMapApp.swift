5ef561d0078d9c436f23bbb20655cf3c6892349a
ArmerDev <Armer.Developer@gmail.com>
2023-06-22 17:54:12 +0100

    Added Day 70 - Adding user locations to a map

--- Day 70/1 - Adding user locations to a map/AddingUserLocationsToAMap/AddingUserLocationsToAMap/AddingUserLocationsToAMapApp.swift
+++ Day 70/1 - Adding user locations to a map/AddingUserLocationsToAMap/AddingUserLocationsToAMap/AddingUserLocationsToAMapApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AddingUserLocationsToAMapApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView3()
         }
     }
 }
