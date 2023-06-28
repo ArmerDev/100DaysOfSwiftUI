e40fb01f5259e7d6530d9d8a8e5713b46be97d3f
ArmerDev <Armer.Developer@gmail.com>
2023-06-28 22:12:01 +0100

    Added Day 79 - Reading custom values from the environment with @EnvironmentObject

---
+++ Day 79/1 - Reading custom values from the environment with @EnvironmentObject/ReadingCustomValuesFromTheEnvironmentWith@EnvironmentObject/ReadingCustomValuesFromTheEnvironmentWith@EnvironmentObject/ReadingCustomValuesFromTheEnvironmentWith_EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingCustomValuesFromTheEnvironmentWith_EnvironmentObjectApp.swift
+//  ReadingCustomValuesFromTheEnvironmentWith@EnvironmentObject
+//
+//  Created by James Armer on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReadingCustomValuesFromTheEnvironmentWith_EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
