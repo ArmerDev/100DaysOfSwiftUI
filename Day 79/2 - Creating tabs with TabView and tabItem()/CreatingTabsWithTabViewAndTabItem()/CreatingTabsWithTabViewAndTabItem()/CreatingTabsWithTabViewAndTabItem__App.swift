c64e16c1a2d70b8ef793c72d90798269b032734c
ArmerDev <Armer.Developer@gmail.com>
2023-06-28 22:35:28 +0100

    Added Day 79 - Creating tabs with TabView and tabItem()

---
+++ Day 79/2 - Creating tabs with TabView and tabItem()/CreatingTabsWithTabViewAndTabItem()/CreatingTabsWithTabViewAndTabItem()/CreatingTabsWithTabViewAndTabItem__App.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingTabsWithTabViewAndTabItem__App.swift
+//  CreatingTabsWithTabViewAndTabItem()
+//
+//  Created by James Armer on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CreatingTabsWithTabViewAndTabItem__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView3()
+        }
+    }
+}
