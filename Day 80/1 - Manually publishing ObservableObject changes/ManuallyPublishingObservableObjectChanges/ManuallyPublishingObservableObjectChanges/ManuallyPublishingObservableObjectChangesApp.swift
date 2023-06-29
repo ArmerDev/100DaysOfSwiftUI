b9593bf006df2b1fc6d73ee821ed75a0c61ecb76
ArmerDev <Armer.Developer@gmail.com>
2023-06-29 13:42:57 +0100

    Added Day 80 - Manually publishing ObservableObject changes

---
+++ Day 80/1 - Manually publishing ObservableObject changes/ManuallyPublishingObservableObjectChanges/ManuallyPublishingObservableObjectChanges/ManuallyPublishingObservableObjectChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManuallyPublishingObservableObjectChangesApp.swift
+//  ManuallyPublishingObservableObjectChanges
+//
+//  Created by James Armer on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ManuallyPublishingObservableObjectChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
