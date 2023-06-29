f96aef85549de3138e97ce6ebb20f7da50622f85
ArmerDev <Armer.Developer@gmail.com>
2023-06-29 19:57:50 +0100

    Added Day 80 - Understanding Swift's Result type

---
+++ Day 80/2 - Understanding Swift’s Result type/UnderstandingSwift’sResultType/UnderstandingSwift’sResultType/UnderstandingSwift_sResultTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingSwift_sResultTypeApp.swift
+//  UnderstandingSwift’sResultType
+//
+//  Created by James Armer on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingSwift_sResultTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
