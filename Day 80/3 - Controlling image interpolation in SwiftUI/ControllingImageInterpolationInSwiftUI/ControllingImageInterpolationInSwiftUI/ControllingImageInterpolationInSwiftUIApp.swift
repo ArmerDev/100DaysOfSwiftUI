435e0311af4ef9d801b5d3acc8570e578d25e8f9
ArmerDev <Armer.Developer@gmail.com>
2023-06-29 20:05:03 +0100

    Added Day 80

---
+++ Day 80/3 - Controlling image interpolation in SwiftUI/ControllingImageInterpolationInSwiftUI/ControllingImageInterpolationInSwiftUI/ControllingImageInterpolationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllingImageInterpolationInSwiftUIApp.swift
+//  ControllingImageInterpolationInSwiftUI
+//
+//  Created by James Armer on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ControllingImageInterpolationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
