e21470df3880065d6f67fb55ee81bebca9cdb2b2
ArmerDev <Armer.Developer@gmail.com>
2023-06-30 20:53:10 +0100

    Added Day 81 - Scheduling local notifications

---
+++ Day 81/3 - Scheduling local notifications/SchedulingLocalNotifications/SchedulingLocalNotifications/SchedulingLocalNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulingLocalNotificationsApp.swift
+//  SchedulingLocalNotifications
+//
+//  Created by James Armer on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SchedulingLocalNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView3()
+        }
+    }
+}
