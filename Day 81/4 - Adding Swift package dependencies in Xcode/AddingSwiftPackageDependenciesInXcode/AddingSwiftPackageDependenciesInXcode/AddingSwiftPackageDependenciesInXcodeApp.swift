16425f41ca638a782bf3820540cb04044408878f
ArmerDev <Armer.Developer@gmail.com>
2023-06-30 21:05:59 +0100

    Added Day 81

---
+++ Day 81/4 - Adding Swift package dependencies in Xcode/AddingSwiftPackageDependenciesInXcode/AddingSwiftPackageDependenciesInXcode/AddingSwiftPackageDependenciesInXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddingSwiftPackageDependenciesInXcodeApp.swift
+//  AddingSwiftPackageDependenciesInXcode
+//
+//  Created by James Armer on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AddingSwiftPackageDependenciesInXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
