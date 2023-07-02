43fca11e176333ba50ee9bc236be4717383dc223
ArmerDev <Armer.Developer@gmail.com>
2023-07-02 21:53:46 +0100

    Added Day 85 - Challenge 1

---
+++ Day 85/Hot Prospects - Challenge/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by James Armer on 02/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
