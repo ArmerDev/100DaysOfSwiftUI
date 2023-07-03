635470da72e088516f94c84534b144a8dbad38f3
ArmerDev <Armer.Developer@gmail.com>
2023-07-03 19:02:32 +0100

    Added Day 86 - Making vibrations with UINotificationFeedbackGenerator and Core Haptics

---
+++ Day 86/2 - Making vibrations with UINotificationFeedbackGenerator and Core Haptics/MakingVibrationsWithUINotificationFeedbackGeneratorAndCoreHaptics/MakingVibrationsWithUINotificationFeedbackGeneratorAndCoreHaptics/MakingVibrationsWithUINotificationFeedbackGeneratorAndCoreHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingVibrationsWithUINotificationFeedbackGeneratorAndCoreHapticsApp.swift
+//  MakingVibrationsWithUINotificationFeedbackGeneratorAndCoreHaptics
+//
+//  Created by James Armer on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MakingVibrationsWithUINotificationFeedbackGeneratorAndCoreHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView4()
+        }
+    }
+}
