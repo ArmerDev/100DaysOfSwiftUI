d022e44e5d810dfe4c720184a06801b1baed7602
ArmerDev <Armer.Developer@gmail.com>
2023-07-04 21:38:44 +0100

    Day 87 - How to be notified when your SwiftUI app moves to the background

---
+++ Day 87/2 - How to be notified when your SwiftUI app moves to the background/HowToBeNotifiedWhenYourSwiftUIAppMovesToTheBackground/HowToBeNotifiedWhenYourSwiftUIAppMovesToTheBackground/HowToBeNotifiedWhenYourSwiftUIAppMovesToTheBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToBeNotifiedWhenYourSwiftUIAppMovesToTheBackgroundApp.swift
+//  HowToBeNotifiedWhenYourSwiftUIAppMovesToTheBackground
+//
+//  Created by James Armer on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HowToBeNotifiedWhenYourSwiftUIAppMovesToTheBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
