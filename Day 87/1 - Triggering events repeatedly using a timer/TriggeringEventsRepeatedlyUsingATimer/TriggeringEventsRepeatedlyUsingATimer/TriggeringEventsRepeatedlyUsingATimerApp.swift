8f64bb2401c7feb41e5b8fbaf33410e73455aedc
ArmerDev <Armer.Developer@gmail.com>
2023-07-04 21:27:56 +0100

    Added Day 87 - Triggering events repeatedly using a timer

---
+++ Day 87/1 - Triggering events repeatedly using a timer/TriggeringEventsRepeatedlyUsingATimer/TriggeringEventsRepeatedlyUsingATimer/TriggeringEventsRepeatedlyUsingATimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriggeringEventsRepeatedlyUsingATimerApp.swift
+//  TriggeringEventsRepeatedlyUsingATimer
+//
+//  Created by James Armer on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TriggeringEventsRepeatedlyUsingATimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
