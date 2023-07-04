45cc2ba9240dc305b932855ebacfd64b4a34368e
ArmerDev <Armer.Developer@gmail.com>
2023-07-04 22:04:57 +0100

    Added Day 87

---
+++ Day 87/3 - Supporting specific accessibility needs with SwiftUI/SupportingSpecificAccessibilityNeedsWithSwiftUI/SupportingSpecificAccessibilityNeedsWithSwiftUI/SupportingSpecificAccessibilityNeedsWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupportingSpecificAccessibilityNeedsWithSwiftUIApp.swift
+//  SupportingSpecificAccessibilityNeedsWithSwiftUI
+//
+//  Created by James Armer on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SupportingSpecificAccessibilityNeedsWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
