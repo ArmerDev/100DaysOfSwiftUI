614f98303262f17053d3ffe5bf3b3ba1bc3314de
ArmerDev <Armer.Developer@gmail.com>
2023-07-09 16:54:17 +0100

    Added Day 92

---
+++ Day 92/3 - How to create a custom alignment guide/HowToCreateACustomAlignmentGuide/HowToCreateACustomAlignmentGuide/HowToCreateACustomAlignmentGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCreateACustomAlignmentGuideApp.swift
+//  HowToCreateACustomAlignmentGuide
+//
+//  Created by James Armer on 09/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HowToCreateACustomAlignmentGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView2()
+        }
+    }
+}
