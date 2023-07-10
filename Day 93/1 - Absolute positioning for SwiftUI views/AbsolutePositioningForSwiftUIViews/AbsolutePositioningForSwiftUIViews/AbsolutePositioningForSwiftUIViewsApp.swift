6577afb6635c6f8caa8fc3e16af9c6b4b60c48db
ArmerDev <Armer.Developer@gmail.com>
2023-07-10 17:46:16 +0100

    Added Day 93 - Absolute positioning for SwiftUI views

---
+++ Day 93/1 - Absolute positioning for SwiftUI views/AbsolutePositioningForSwiftUIViews/AbsolutePositioningForSwiftUIViews/AbsolutePositioningForSwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsolutePositioningForSwiftUIViewsApp.swift
+//  AbsolutePositioningForSwiftUIViews
+//
+//  Created by James Armer on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsolutePositioningForSwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
