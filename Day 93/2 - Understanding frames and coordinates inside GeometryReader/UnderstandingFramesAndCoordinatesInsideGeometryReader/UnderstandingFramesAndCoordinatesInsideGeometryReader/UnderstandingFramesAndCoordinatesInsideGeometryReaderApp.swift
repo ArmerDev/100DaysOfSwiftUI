7dc4dda81e75b7c6994ca7afe3e46e332ffb4571
ArmerDev <Armer.Developer@gmail.com>
2023-07-10 18:08:14 +0100

    Added Day 93 - Understanding frames and coordinates inside GeometryReader

---
+++ Day 93/2 - Understanding frames and coordinates inside GeometryReader/UnderstandingFramesAndCoordinatesInsideGeometryReader/UnderstandingFramesAndCoordinatesInsideGeometryReader/UnderstandingFramesAndCoordinatesInsideGeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingFramesAndCoordinatesInsideGeometryReaderApp.swift
+//  UnderstandingFramesAndCoordinatesInsideGeometryReader
+//
+//  Created by James Armer on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingFramesAndCoordinatesInsideGeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView3()
+        }
+    }
+}
