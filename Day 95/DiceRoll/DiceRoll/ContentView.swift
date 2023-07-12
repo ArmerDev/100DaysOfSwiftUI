8f59568fae5fc0b7a55a5da9901826537cc72b7a
ArmerDev <Armer.Developer@gmail.com>
2023-07-12 22:39:56 +0100

    Working on Day 95 Challenge

---
+++ Day 95/DiceRoll/DiceRoll/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  DiceRoll
+//
+//  Created by James Armer on 12/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    @State private var rollTotal: Int? = nil
+    @State private var diceImage: String? = nil
+    @State private var rollingCounter = 0
+    @State private var speed: rollSpeed = .fast
+    @State private var speedValue: Double = 0.1
+    
+    init() {
+        _timer = State(initialValue: Timer.publish(every: speedValue, on: .main, in: .common).autoconnect())
+    }
+    
+    enum rollSpeed: Double {
+        case fast = 0.1
+        case medium = 0.5
+        case slow = 0.9
+    }
+    
+    var body: some View {
+        VStack {
+            if let total = rollTotal {
+                Text("\(total)")
+                    .font(.largeTitle)
+            } else {
+                Text(" ")
+                    .font(.largeTitle)
+            }
+            
+            Spacer()
+            
+            if let image = diceImage {
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+            }
+            
+            Spacer()
+            
+            Button("Roll Dice") {
+                rollingCounter = 0
+                timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+            }
+            .buttonStyle(.borderedProminent)
+            .onReceive(timer) { _ in
+                rollDice()
+            }
+        }
+        .padding()
+    }
+    
+    func rollDice() {
+        
+        speedValue += 0.05
+        
+        rollingCounter += 1
+
+        if rollingCounter == 10 {
+            speed = .medium
+        } else if rollingCounter == 20 {
+            speed = .slow
+        } else if rollingCounter == 30 {
+            self.timer.upstream.connect().cancel()
+        }
+        
+        rollTotal = Int.random(in: 1...6)
+        diceImage = "\(rollTotal!)"
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 95/DiceRoll/DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by James Armer on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
