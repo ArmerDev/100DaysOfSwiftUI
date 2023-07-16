3ee5d99d4f4458217bd7c4c742a5eb7a5de727ae
ArmerDev <Armer.Developer@gmail.com>
2023-07-16 21:56:56 +0100

    Added Day 96 - Using groups as transparent layout containers

---
+++ Day 96/3 - Using groups as transparent layout containers/UsingGroupsAsTransparentLayoutContainers/UsingGroupsAsTransparentLayoutContainers/UsingGroupsAsTransparentLayoutContainersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingGroupsAsTransparentLayoutContainersApp.swift
+//  UsingGroupsAsTransparentLayoutContainers
+//
+//  Created by James Armer on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct UsingGroupsAsTransparentLayoutContainersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView3()
+        }
+    }
+}
