79fb9e7a1f08afdc5c52e4fbf393b1a8dfd5b921
ArmerDev <Armer.Developer@gmail.com>
2023-07-16 10:27:20 +0100

    Added Day 96 - Working with two side by side views in SwiftUI

---
+++ Day 96/1 - Working with two side by side views in SwiftUI/WorkingWithTwoSideBySideViewsInSwiftUI/WorkingWithTwoSideBySideViewsInSwiftUI/WorkingWithTwoSideBySideViewsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithTwoSideBySideViewsInSwiftUIApp.swift
+//  WorkingWithTwoSideBySideViewsInSwiftUI
+//
+//  Created by James Armer on 16/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithTwoSideBySideViewsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView4()
+        }
+    }
+}
